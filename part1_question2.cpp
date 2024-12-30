c2828a3be44043d7e64b6139bc90c447c18e4161
20221052 <139171221+20221052@users.noreply.github.com>
2024-12-30 03:09:50 -0800

    Update part1_question2.cpp

--- part1_question2.cpp
+++ part1_question2.cpp
@@ -1,14 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int solve(string s){
-    if(s.find('m')!=-1||s.find('w')!=-1) return 0;
-    int c=0;
-    for (int i = 0; i < s.size()-1; ++i) {
-        if(s[i]=='u'&&s[i+1]=='u'||s[i]=='n'&&s[i+1]=='n') c++;
+const int MOD = 1e9 + 7;
+
+int solve(string s) {
+     if (s.find('m') != string::npos || s.find('w') != string::npos) 
+        return 0;
+    int n = s.size();
+    vector<long long> ways(n + 1, 0); // ways[i]: number of ways to interpret s[0:i-1]
+    ways[0] = 1; 
+    for (int i = 1; i <= n; ++i) {
+        ways[i] = ways[i - 1]; 
+        if (i > 1 && (s[i - 1] == 'u' && s[i - 2] == 'u' || s[i - 1] == 'n' && s[i - 2] == 'n')) {
+            ways[i] = (ways[i] + ways[i - 2]) % MOD; 
+        }
     }
-    return c+1;
+
+    return ways[n];
 }
-int main(){
-    string s; cin>>s;
-    cout<<solve(s);
+
+int main() {
+    string s;
+    cin >> s;
+    cout << solve(s) << endl;
+    return 0;
 }
